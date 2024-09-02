34b36103b3d234aa3bd198abb5fc6945623d5525
Moses Dev <mosesdev777@gmail.com>
2024-09-02 10:35:40 -0400

    Modifying Character model to adding created_at field

--- contentflow/models.py
+++ contentflow/models.py
@@ -232,7 +232,9 @@ class Character(models.Model):
     channel = models.ManyToManyField(MediaPlatform, null=False, blank=False)
     name = models.CharField(max_length=200, null=False, blank=False)
     character_type = models.CharField(max_length=200, null=False, blank=False, choices=TYPE_CHOICES)
+    created_at = models.DateTimeField(auto_now_add=True)
     
     def __str__(self):
         return f"{self.name} - {self.channel.name}"
-    
+
+
