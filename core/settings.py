65f34265771c8bb7d13c9488bdd3179f66fa716a
Moses Dev <mosesdev777@gmail.com>
2024-12-17 10:31:41 -0400

    removing livereload from installed apps again

--- core/settings.py
+++ core/settings.py
@@ -27,9 +27,6 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    
-    'livereload', # Development
-    
     'contentflow'
 ]
 
