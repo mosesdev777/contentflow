27b132b2f327393c538a37c2a9c9159027dbc6da
Moses Dev <mosesdev777@gmail.com>
2025-04-04 10:58:44 -0400

    modificando anchura del formulario prompt

--- contentflow/forms.py
+++ contentflow/forms.py
@@ -188,7 +188,7 @@ class PromptForm(forms.ModelForm):
         widgets = {
             'channel': forms.Select(attrs={'class':'form-select my-2'}),
             'content_format': forms.Select(attrs={'class':'form-select my-2'}),
-            'text': SummernoteWidget(attrs={'summernote': {'width': '100    %', 'height': '400px'}}) #forms.Textarea(attrs={'class':'form-select my-2', 'placeholder':'Please enter your prompt here'}),
+            'text': SummernoteWidget(attrs={'summernote': {'width': '100%', 'height': '400px'}}) #forms.Textarea(attrs={'class':'form-select my-2', 'placeholder':'Please enter your prompt here'}),
         }
 
 class TitleForm(forms.ModelForm):
