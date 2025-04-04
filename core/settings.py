3df32e72d861d00a2eb01af73992c842e647a6de
Moses Dev <mosesdev777@gmail.com>
2025-04-04 01:17:20 -0400

    agregando django-ckeditor

--- core/settings.py
+++ core/settings.py
@@ -15,7 +15,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-2f$i40m$_4h0vhyin0rv+=-bq-k8nc&u^wzm!n@1$%5c&^2u9h'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = [
     'contentflow-jih0.onrender.com',
@@ -129,3 +129,5 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 MEDIA_ROOT = 'media'
 MEDIA_URL = '/media/'
+
+CKEDITOR_UPLOAD_PATH = "uploads/"
--- core/urls.py
+++ core/urls.py
@@ -6,7 +6,8 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('contentflow.urls'))
+    path('', include('contentflow.urls')),
+    path('ckeditor/', include('ckeditor_uploader.urls')),
 ]
 
 
